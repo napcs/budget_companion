e92e48d1c1a95d0d077986374c10db46a9a5c5a3
Brian Hogan <brianhogan@napcs.com>
2015-11-08 14:13:31 -0600

    removed income_payments

--- app/helpers/income_payments_helper.rb
+++
@@ -1,2 +0,0 @@
-module IncomePaymentsHelper
-end
--- app/models/income_payment.rb
+++
@@ -1,6 +0,0 @@
-class IncomePayment < ActiveRecord::Base
-  belongs_to :account
-  belongs_to :category
-  belongs_to :incomePayable, polymorphic: true
-  
-end
